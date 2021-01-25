d6f5d09088a38150bfbaea9f6ad5b3326ff92a25
Nadine Strobel <69895853+Nadine1111@users.noreply.github.com>
2021-01-25 12:55:11 +0100

    fullscreen

---
+++ src/components/Typeform4/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { ReactTypeformEmbed } from "react-typeform-embed";
+
+function TypeformFullScreen() {
+  return (
+    <div className="App">
+      <ReactTypeformEmbed url="https://bmd9bai2izv.typeform.com/to/BYkr9iLP" />
+    </div>
+  );
+}
+
+export default TypeformFullScreen;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Typeform from './components/Typeform4';
+import TypeformFullScreen from './components/Typeform4';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <TypeformFullScreen />
   </React.StrictMode>,
   document.getElementById('root')
 );
